1bc1334067d043d628f6db881603a31d0330e662
avinashreddy02 <avinashreddyp02@gmail.com>
2020-11-17 19:16:48 -0700

    code changes
    code changes

--- train.py
+++ train.py
@@ -14,15 +14,15 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+ds = TabularDatasetFactory.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
 
-x, y = clean_data(ds)
+
 
 # TODO: Split data into train and test sets.
 
 ### YOUR CODE HERE ###a
 
-run = Run.get_context()
+
 
 def clean_data(data):
     # Dict for cleaning data
@@ -50,6 +50,8 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
+    return x_df, y_df
+    
 
 def main():
     # Add arguments to script
@@ -67,6 +69,12 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+    
+x, y = clean_data(ds)
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.3,random_state=42)
+
+run = Run.get_context()
 
 if __name__ == '__main__':
     main()
